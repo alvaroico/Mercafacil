d47a242f456cfc65bc11fced4125e3b7d50c854b
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2021-09-01 21:21:04 -0300

    criarTabelas script

---
+++ src/CreateTabelas.ts
@@ -0,0 +1,34 @@
+import { InsertMYsql } from "./connection/Mysql";
+import { PoolPostgreSQL } from "./connection/PostgreSQL";
+
+const job = async () => {
+  await PoolPostgreSQL(`CREATE table contacts (
+	id serial PRIMARY KEY,
+	nome VARCHAR ( 200 ) NOT NULL,
+	celular VARCHAR ( 20 ) NOT NULL
+);  
+`)
+    .then(async (resultado) => {
+      // console.log(resultado);
+      console.info("Tabela no PostgreSQL criada com sucesso!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  await InsertMYsql(`CREATE table contacts (
+	id serial PRIMARY KEY,
+	nome VARCHAR ( 100 ) NOT NULL,
+	celular VARCHAR ( 13 ) NOT NULL
+);
+`)
+    .then(async (resultado) => {
+      // console.log(resultado);
+      console.info("Tabela no MySql criada com sucesso!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  process.exit(1);
+};
+job();
