5497cbb86f285e9f73e06f490ccd2f544443f31e
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2021-09-01 21:47:33 -0300

    Criado Rotinas finais e adicionado GIF ao README

--- src/CreateTabelas.ts
+++ src/CreateTabelas.ts
@@ -3,11 +3,10 @@ import { PoolPostgreSQL } from "./connection/PostgreSQL";
 
 const job = async () => {
   await PoolPostgreSQL(`CREATE table contacts (
-	id serial PRIMARY KEY,
-	nome VARCHAR ( 200 ) NOT NULL,
-	celular VARCHAR ( 20 ) NOT NULL
-);  
-`)
+    id serial PRIMARY KEY,
+    nome VARCHAR ( 100 ) NOT NULL,
+    celular VARCHAR ( 13 ) NOT NULL
+  );`)
     .then(async (resultado) => {
       // console.log(resultado);
       console.info("Tabela no PostgreSQL criada com sucesso!");
@@ -16,10 +15,10 @@ const job = async () => {
       console.log(err);
     });
   await InsertMYsql(`CREATE table contacts (
-	id serial PRIMARY KEY,
-	nome VARCHAR ( 100 ) NOT NULL,
-	celular VARCHAR ( 13 ) NOT NULL
-);
+    id serial PRIMARY KEY,
+    nome VARCHAR ( 200 ) NOT NULL,
+    celular VARCHAR ( 20 ) NOT NULL
+  )
 `)
     .then(async (resultado) => {
       // console.log(resultado);
